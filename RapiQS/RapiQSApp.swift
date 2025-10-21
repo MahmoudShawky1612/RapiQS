22f08d1591924cdf8d4c7a6fad3b97b8e2b0dc7e
Dodje <nostyy30@gmail.com>
2025-10-21 06:14:57 +0300

    Initial Commit

---
+++ RapiQS/RapiQSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RapiQSApp.swift
+//  RapiQS
+//
+//  Created by DODJE on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct RapiQSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RapiQSTests/RapiQSTests.swift
@@ -0,0 +1,17 @@
+//
+//  RapiQSTests.swift
+//  RapiQSTests
+//
+//  Created by DODJE on 21/10/2025.
+//
+
+import Testing
+@testable import RapiQS
+
+struct RapiQSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
